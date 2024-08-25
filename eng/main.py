0b0bb58e838ca770d0552e3c7e79aa8389b0afda
KatBob <126419799+KatBob132@users.noreply.github.com>
2024-08-24 19:01:05 -0500

    revamp again

--- eng/main.py
+++ eng/main.py
@@ -5,13 +5,14 @@ from scp.fnc import *
 from pygame.locals import *
 import pygame as pg
 
+from calendar import monthrange as mon
 from time import time as tme
 from sys import exit as ext
 
 clr = jsn_dat("eng/dat/clr.json")
 scn = {}
 
-scn["siz"] = (256, 256)
+scn["siz"] = (384, 384)
 scn["pix-siz"] = 3
 
 scn["srf"] = pg.Surface(scn["siz"])
@@ -33,47 +34,26 @@ fps_cal["fps"] = 0
 fps_cal["fps-lit"] = []
 fps_cal["fps-avg"] = 0
 
-# red green blue orange pink
-chc_clr = clr["red"]
-bck_clr = clr["white"]
-for_clr = clr["black"]
-
 # Faraway Alarm
 # Today Alarm
 # Daily Alarm
 # View Alarms
 
-btt = btt_obj(chc_clr, bck_clr, for_clr, 0.2, 0.2, scn["srf"], txt, 2, "menu")
-
-btt.add_grp("menu")
-btt.add_grp("faraway")
-# btt.add_grp("today")
-# btt.add_grp("daily")
-# btt.add_grp("view")
-
-btt.add_btt("menu", "Faraway", (128, 128 - 41), (True, True))
-btt.add_btt("menu", "Today", (128, 128 - 14), (True, True))
-btt.add_btt("menu", "Daily", (128, 128 + 13), (True, True))
-btt.add_btt("menu", "View", (128, 128 + 38), (True, True))
-
-btt.add_btt("faraway", "5", (128, 128 - 41), (True, True))
-btt.add_btt("faraway", "4", (128, 128 - 14), (True, True))
-btt.add_btt("faraway", "3", (128, 128 + 13), (True, True))
-btt.add_btt("faraway", "1", (128, 128 + 38), (True, True))
-
 pg.init()
 dpy = pg.display.set_mode(siz, DOUBLEBUF)
 pg.display.set_caption("Alzhe Timer")
+pg.display.set_icon(pg.transform.scale(pg.image.load("eng/icon.png"), (2560, 2560)))
 fps = pg.time.Clock()
 
 mos_dat = {}
 
 mos_dat["xy"] = (pg.mouse.get_pos()[0] // scn["pix-siz"], pg.mouse.get_pos()[1] // scn["pix-siz"])
 mos_dat["btt"] = [False, False, False]
+mos_dat["inc"] = 1
 
 while True:
-    dpy.fill(bck_clr)
-    scn["srf"].fill(bck_clr)
+    dpy.fill(clr["white"])
+    scn["srf"].fill(clr["white"])
 
     pg.mouse.set_visible(False)
 
@@ -88,21 +68,14 @@ while True:
 
     while len(fps_cal["fps-lit"]) > round(fps_cal["fps"]):
         fps_cal["fps-lit"].pop(0)
-    
+
     try:
         fps_cal["fps-avg"] = round(sum(fps_cal["fps-lit"]) / len(fps_cal["fps-lit"]))
     except:
-        fps_cal["fps-avg"] = round(fps_cal["fps "])
+        fps_cal["fps-avg"] = round(fps_cal["fps"])
 
-    # pg.draw.rect(scn["srf"], clr["green"], pg.Rect(scn["siz"][0] / 2, 0, 1, scn["siz"][1]))
-    # pg.draw.rect(scn["srf"], clr["green"], pg.Rect(0, scn["siz"][1] / 2, scn["siz"][0], 1))
-
-    btt.upt(tme_cal["dlt"], mos_dat["xy"])
-
-    pg.draw.rect(scn["srf"], clr["green"], pg.Rect(mos_dat["xy"][0] - 1, mos_dat["xy"][1] - 1, 3, 3))
-
-    txt.drw("Alzhe", (2, 2), 3, chc_clr, (0, 1), for_clr)
-    txt.drw("Timer", (2, 26), 3, for_clr, (0, 1), chc_clr)
+    pg.draw.rect(scn["srf"], clr["green"], pg.Rect(scn["siz"][0] / 2, 0, 1, scn["siz"][1]))
+    pg.draw.rect(scn["srf"], clr["green"], pg.Rect(0, scn["siz"][1] / 2, scn["siz"][0], 1))
 
     for evt in pg.event.get():
         if evt.type == pg.QUIT:
@@ -113,11 +86,8 @@ while True:
             if evt.key == pg.K_ESCAPE:
                 pg.quit()
                 ext()
-            
-            if evt.key == pg.K_SPACE:
-                upt = not upt
 
-        if evt.type == pg.MOUSEBUTTONDOWN:        
+        if evt.type == pg.MOUSEBUTTONDOWN:
             for a in range(3):
                 if evt.button == a + 1:
                     mos_dat["btt"][a] = True
@@ -126,7 +96,7 @@ while True:
             for a in range(3):
                 if evt.button == a + 1:
                     mos_dat["btt"][a] = False
-     
+    
     scn["win"] = pg.transform.scale(scn["srf"], siz)
     scn["win-bck"] = pg.transform.scale(scn["srf"], siz)
 
--- eng/scp/fnc.py
+++ eng/scp/fnc.py
@@ -23,23 +23,3 @@ def jsn_dat(loc):
 
 def cfg_clr(clr):
     return (clp(int(clr[0]), 0, 255), clp(int(clr[1]), 0, 255), clp(int(clr[2]), 0, 255))
-
-def bnd_clr(clr_1, clr_2, wgt_1, wgt_2):
-    clr_1 = cfg_clr(clr_1)
-    clr_2 = cfg_clr(clr_2)
-
-    wgt_1 = int(wgt_1)
-    wgt_2 = int(wgt_2)
-
-    clr_ret = [0, 0, 0]
-
-    for a in range(3):
-        for b in range(wgt_1):
-            clr_ret[a] += clr_1[a]
-        
-        for c in range(wgt_2):
-            clr_ret[a] += clr_2[a]
-        
-        clr_ret[a] = round(clr_ret[a] / (wgt_1  + wgt_2))
-
-    return clr_ret
